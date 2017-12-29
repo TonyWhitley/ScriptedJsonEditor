0ad69b0cb48e799b793a906b9568a16d92b93c88
TonyWhitley <Tony_Whitley@hotmail.com>
2017-12-29 16:34:36 +0000

    (Makeexe.bat: extending the path TWICE doesn't work for reasons that aren't clear)

--- ScriptedJsonEditor/makeexe.bat
+++ ScriptedJsonEditor/makeexe.bat
@@ -1,13 +1,14 @@
 setlocal
-set path=%path%;"C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64"
+path=%path%;C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64;env\Scripts
 
 if exist env\scripts (
-	set path=%path%;env\Scripts
 	pyinstaller ^
 	  --onefile ^
 	  --distpath . ^
 	  "%~dp0\ScriptedJsonEditor.py "
 	pause
+	) else (
+	rem No virtualenv
 	)
 REM fails to get pypiwin32 on AppVeyor ####  if not exist env\scripts	pip install -r requirements.txt
 
