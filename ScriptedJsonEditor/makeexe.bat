ae0ae3dfc4a7aa9b96cd361e4ffb4439da1be2cb
TonyWhitley <Tony_Whitley@hotmail.com>
2017-12-29 15:08:15 +0000

    (PyInstaller - AppVeyor fails to get pypiwin32 so don't bother trying)

--- ScriptedJsonEditor/makeexe.bat
+++ ScriptedJsonEditor/makeexe.bat
@@ -1,11 +1,13 @@
 setlocal
 set path=%path%;"C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64"
 
-if exist env\scripts 	set path=%path%;env\Scripts
-if not exist env\scripts	pip install -r requirements.txt
+if exist env\scripts (
+	set path=%path%;env\Scripts
+	pyinstaller ^
+	  --onefile ^
+	  --distpath . ^
+	  "%~dp0\ScriptedJsonEditor.py "
+	pause
+	)
+REM fails to get pypiwin32 on AppVeyor ####  if not exist env\scripts	pip install -r requirements.txt
 
-pyinstaller ^
-  --onefile ^
-  --distpath . ^
-  "%~dp0\ScriptedJsonEditor.py "
-pause
